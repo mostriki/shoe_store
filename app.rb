10e18e831551e6ce167f003bcaad728e936dc8b2
Riki Montgomery <mostriki820@gmail.com>
2017-09-29 11:56:43 -0700

    started routes and layout

--- app.rb
+++ app.rb
@@ -2,3 +2,27 @@ require("bundler/setup")
 Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+
+get('/') do
+  @stores = Store.all()
+  erb(:index)
+end
+
+post('/') do
+  name = params['name']
+  @stores = Store.create({name: name})
+  erb(:index)
+end
+
+get("/store/:id") do
+  @store = Project.find(params['id'].to_i())
+  erb(:store)
+end
+
+post("/store/:id") do
+  name = params['name']
+  volunteer = Shoe.create({brand: brand, price: price})
+  @store = Project.find(params['id'].to_i())
+  erb(:store)
+end
